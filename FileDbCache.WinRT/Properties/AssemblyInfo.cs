befa16c401e933e56a74ec2f55d73c6d5682057f
Artem-PC <xtergs@gmail.com>
2015-02-21 12:01:12 +0300

    map Cache

---
+++ FileDbCache.WinRT/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XAML Map Control FileDbCache (WinRT)")]
+[assembly: AssemblyDescription("IImageCache implementation based on EzTools FileDb")]
+[assembly: AssemblyProduct("XAML Map Control")]
+[assembly: AssemblyCompany("Clemens Fischer")]
+[assembly: AssemblyCopyright("© 2015 Clemens Fischer")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("2.4.7")]
+[assembly: AssemblyFileVersion("2.4.7")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
---
+++ ImageFileCache.WinRT/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XAML Map Control ImageFileCache (WinRT)")]
+[assembly: AssemblyDescription("IImageCache implementation based on local image files")]
+[assembly: AssemblyProduct("XAML Map Control")]
+[assembly: AssemblyCompany("Clemens Fischer")]
+[assembly: AssemblyCopyright("© 2015 Clemens Fischer")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("2.4.7")]
+[assembly: AssemblyFileVersion("2.4.7")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]

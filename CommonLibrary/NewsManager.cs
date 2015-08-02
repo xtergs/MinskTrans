fbddf5c20aa2a7e89eb5fb0b46baacfc4819fa16
Artem-PC <xtergs@gmail.com>
2015-08-02 19:45:03 +0300

    Work!!!

--- CommonLibrary/NewsManager.cs
+++ CommonLibrary/NewsManager.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using Windows.Storage;
 using MinskTrans.DesctopClient.Modelview;
 using MinskTrans.Net;
-using MinskTrans.Universal.Annotations;
 using MinskTrans.Utilites.Base.IO;
 using MinskTrans.Utilites.Base.Net;
 using MyLibrary;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace CommonLibrary
 {
@@ -29,13 +20,9 @@ namespace CommonLibrary
 			
 		}
 
-
-        #region Overrides of NewsManagerBase
-
         private ApplicationSettingsHelper lastUpdateDataDateTimeBack;
         public override DateTime LastUpdateMainNewsDateTimeUtc
         {
-#if WINDOWS_PHONE_APP || WINDOWS_UAP
             get
             {
                 if (lastUpdateDataDateTimeBack == null)
@@ -50,15 +37,12 @@ namespace CommonLibrary
                 lastUpdateDataDateTimeBack.DateTimeSettings = value;
                 OnPropertyChanged();
             }
-#else
-			get { return allNews.Max(x => x.PostedUtc); }
-#endif
         }
 
         private ApplicationSettingsHelper lastUpdateHotDataDateTimeBack;
         public override DateTime LastUpdateHotNewsDateTimeUtc
         {
-#if WINDOWS_PHONE_APP || WINDOWS_UAP
+
 			get
 			{
 				if (lastUpdateHotDataDateTimeBack == null)
@@ -73,10 +57,6 @@ namespace CommonLibrary
 				lastUpdateHotDataDateTimeBack.DateTimeSettings = value;
 				OnPropertyChanged();
 			}
-#else
-            get { return allHotNewsDictionary.Max(x => x.CollectedUtc); }
-
-#endif
         }
 
         public override Task<List<NewsEntry>> CheckAsync(string uri, string XpathSelectInfo, string XpathSelectDate)
@@ -88,7 +68,5 @@ namespace CommonLibrary
 		{
 			throw new NotImplementedException();
 		}
-
-		#endregion
 	}
 }

bdaf6e334315931ab2b920b866c789cee6969312
xtergs <xtergs@gmail.com>
2015-11-18 20:15:26 +0300

    changed key

--- MinskTrans/Modelview/SettingsModelView.cs
+++ MinskTrans/Modelview/SettingsModelView.cs
@@ -30,14 +30,15 @@ using MinskTrans.Universal.Annotations;
         // DateTime DateTimeSettings { get; set; }
         void SimpleSet<T>(T value, [CallerMemberName] string key = null);
         T SimpleGet<T>(T defValue = default(T), [CallerMemberName] string key = null);
-        //void SimpleSet(string value, [CallerMemberName] string key = null);
-        //void SimpleSet(bool value, [CallerMemberName] string key = null);
-        //void SimpleSet(int value, [CallerMemberName] string key = null);
-        //string SimleGet(string defValue = null, [CallerMemberName] string key = null);
-        //object SimleGet(object defValue = null, [CallerMemberName] string key = null);
-        //bool SimleGet(bool defValue = true, [CallerMemberName] string key = null);
-        //int SimleGet(int defValue = 0, [CallerMemberName] string key = null);
-        DateTime SimbleGet(DateTime value = default(DateTime), [CallerMemberName] string key = null);
+	    Error SimpleGet(Error defValue = default(Error), [CallerMemberName] string key = null);
+		//void SimpleSet(string value, [CallerMemberName] string key = null);
+		//void SimpleSet(bool value, [CallerMemberName] string key = null);
+		//void SimpleSet(int value, [CallerMemberName] string key = null);
+		//string SimleGet(string defValue = null, [CallerMemberName] string key = null);
+		//object SimleGet(object defValue = null, [CallerMemberName] string key = null);
+		//bool SimleGet(bool defValue = true, [CallerMemberName] string key = null);
+		//int SimleGet(int defValue = 0, [CallerMemberName] string key = null);
+		DateTime SimbleGet(DateTime value = default(DateTime), [CallerMemberName] string key = null);
         void SimpleSet(DateTime value, [CallerMemberName] string key = null);
     }
 
@@ -80,7 +81,14 @@ using MinskTrans.Universal.Annotations;
             return (T)ApplicationData.Current.LocalSettings.Values[key];
         }
 
-        public void SimpleSet(DateTime value, [CallerMemberName] string key = null)
+		public Error SimpleGet(Error defValue = default(Error), [CallerMemberName] string key = null)
+		{
+			if (!ApplicationData.Current.LocalSettings.Values.ContainsKey(key))
+				ApplicationData.Current.LocalSettings.Values.Add(key, (int)defValue);
+			return (Error)ApplicationData.Current.LocalSettings.Values[key];
+		}
+
+		public void SimpleSet(DateTime value, [CallerMemberName] string key = null)
         {
             if (dateTimeDictionary.ContainsKey(key) && dateTimeDictionary[key] == value)
                 return;

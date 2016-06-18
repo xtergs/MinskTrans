50109229e0f6195522e53f1bcb84808ba7f478cc
xtergs <xtergs@gmail.com>
2016-06-18 18:18:34 +0300

    asdf

--- UniversalMinskTrans/UniversalMinskTransRelease/ViewModel/MainViewModel.cs
+++ UniversalMinskTrans/UniversalMinskTransRelease/ViewModel/MainViewModel.cs
@@ -1,6 +1,6 @@
 using GalaSoft.MvvmLight;
 
-namespace UniversalMinskTrans.ViewModel
+namespace UniversalMinskTransRelease.ViewModel
 {
     /// <summary>
     /// This class contains properties that the main View can data bind to.
--- UniversalMinskTrans/UniversalMinskTransRelease/ViewModel/ViewModelLocator.cs
+++ UniversalMinskTrans/UniversalMinskTransRelease/ViewModel/ViewModelLocator.cs
@@ -1,7 +1,7 @@
 /*
   In App.xaml:
   <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:UniversalMinskTrans"
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:UniversalMinskTransRelease"
                            x:Key="Locator" />
   </Application.Resources>
   
@@ -16,7 +16,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
 
-namespace UniversalMinskTrans.ViewModel
+namespace UniversalMinskTransRelease.ViewModel
 {
     /// <summary>
     /// This class contains static references to all the view models in the
